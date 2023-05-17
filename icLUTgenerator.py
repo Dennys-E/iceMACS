3660e65a4e1ab12d628b738f3d59c155ef3d2186
Dennys.Erdtmann <dennys.erdtmann@met-ws-740m01.meteo.physik.uni-muenchen.de>
2023-05-17 20:11:58 +0200

    Hopefully improved LUTgenerator

--- icLUTgenerator.py
+++ icLUTgenerator.py
@@ -68,32 +68,6 @@ def save_uvspec_output_under(input_file_path, output_file_path):
     return
 
 
-def get_uvspec_output(input_file_path, temp_path):
-    
-    """To ensure correct format of returned file, uvspec output is temporarily saved in specified directory. If argument delete 
-    is True, this file is cleaned before function closes. Argument uniqueness was implemented to protect the temporary file if 
-    function is run multiple times in parallel.
-    """
-    
-    f = open(input_file_path, 'r')
-    input_file = f.read()
-    f.close()
-
-    result = subprocess.run([UVSPEC_PATH], input = input_file, capture_output=True, encoding='ascii')
-
-    output_temp = result.stdout
-    
-    f = open(temp_path, 'w')
-    f.write(output_temp)
-    f.close()
-    
-    f = open(temp_path, 'r')
-    return_value = np.loadtxt(f)
-    f.close()
-    
-    return return_value
-
-
 def get_index_combinations(length):
     
     """Returns all relevant index combinations for two arrays (e.g. of cloud parameters) with the same length, while exluding 
@@ -128,6 +102,68 @@ def get_measurement_arrays(measurementLUT, wvl1, wvl2):
         reflectivity_array[line,1]=LUTcut.isel(r_eff = ir_eff, tau550=itau550).sel(wvl=wvl2).reflectivity.values
     
     return reflectivity_array, cloud_param_array
+
+
+def get_uvspec_output(input_file_path, temp_path):
+    
+    """To ensure correct format of returned file, uvspec output is temporarily saved in specified directory. If argument delete 
+    is True, this file is cleaned before function closes. Argument uniqueness was implemented to protect the temporary file if 
+    function is run multiple times in parallel.
+    """
+    
+    f = open(input_file_path, 'r')
+    input_file = f.read()
+    f.close()
+
+    result = subprocess.run([UVSPEC_PATH], input = input_file, 
+                            capture_output=True, encoding='ascii')
+
+    output_temp = result.stdout
+    
+    f = open(temp_path, 'w')
+    f.write(output_temp)
+    f.close()
+    
+    f = open(temp_path, 'r')
+    return_value = np.loadtxt(f)
+    f.close()
+    
+    return return_value
+
+
+def get_formatted_uvspec_output(input_file_path, nwvl, numu, nphi, temp_path):
+    
+    """To ensure correct format of returned file, uvspec output is temporarily saved in specified directory. If argument delete 
+    is True, this file is cleaned before function closes. Argument uniqueness was implemented to protect the temporary file if 
+    function is run multiple times in parallel.
+    """
+    
+    f = open(input_file_path, 'r')
+    input_file = f.read()
+    f.close()
+
+    result = subprocess.run([UVSPEC_PATH], input = input_file, capture_output=True, encoding='ascii')
+
+    output_temp = result.stdout
+    
+    f = open(temp_path, 'w')
+    f.write(output_temp)
+    f.close()
+    
+    f = open(temp_path, 'r')
+    return_value = np.loadtxt(f)
+    f.close()
+    
+    result = np.zeros((nwvl, nphi, numu))
+    
+    for iwvl in range(nwvl):
+        for iumu in range(numu):
+            for iphi in range(nphi):
+                result[iwvl, iphi, iumu] = \
+                return_value[iwvl, iumu*nphi + iphi + 1] 
+    
+    return result
+
 
 
 def get_ic_reflectivity(args):
@@ -189,7 +225,18 @@ def get_ic_reflectivity(args):
     input_file_template_path = INPUT_FILES_DIR+'/ic_input_file_template.txt'
                             
     write_input_file(input_file_template_path, generated_input_file_path, input_file_args)
-    uvspec_result = get_uvspec_output(generated_input_file_path, temp_dir_path+'/temp_output.txt')
+    
+    nwvl = len(wvl_array)
+    numu = len(umu_array)
+    nphi=len(phi_array)
+    
+    uvspec_result = get_formatted_uvspec_output(generated_input_file_path,
+                                                nwvl, numu, nphi,
+                                                temp_dir_path+'/temp_output.txt') 
+    """
+    uvspec_result = get_uvspec_output(generated_input_file_path,
+                                                temp_dir_path+'/temp_output.txt')
+    """
         
     # Delete tree of temporary dir
     shutil.rmtree(temp_dir_path, ignore_errors=True)
@@ -197,89 +244,102 @@ def get_ic_reflectivity(args):
     return uvspec_result
 
 
-def write_icLUT(LUTpath, wvl_array, phi_array, umu_array, sza_array, r_eff_array, tau550_array, ic_habit_array,
-              phi0=0, cloud_top_distance=1, ic_properties = "baum_v36", delete=True, CPUs=8, description=""):
-    
-    """Generates lookup table for specified ranges and grid points and writes file as netCDF to specified paths. Cloud file 
-    format and positon as well as uvspec input file template are specified in the function.Default distance from cloud is 1km. 
-    ic_properties can also be changed as keyword argument, otherwise baum_v36 is default.'CPUs' gives number of cores on which 
-    the function is run in parallel.
-    """
+def new_write_icLUT(LUTpath, wvl_array, phi_array, umu_array, sza_array, 
+                    r_eff_array, tau550_array, ic_habit_array, phi0=0, 
+                    cloud_top_distance=1, ic_properties = "baum_v36", 
+                    CPUs=8, description=""):
     
     start_time = timer()
-    print("Format input...")
-    
-    # Define specific temporary file name marker. This was implemented to be able to run function multiple times in parallel. 
-    extension = ".nc"
-    if LUTpath.endswith(extension):
-        LUTname = "_"+LUTpath.replace(extension,'')
-    else:
-        print("Argument LUTpath has to have .nc extension! Program might fail...")
-        
-    if "/" in LUTname:
-        LUTname = LUTname.replace("/",'_')
-
-    wvl_grid_file_path = 'InputFiles/temp/wvlLUT'+LUTname+'.txt'
+    temp_dir_path = tempfile.mkdtemp()
+    wvl_grid_file_path = temp_dir_path+'/wvl_grid_file.txt'
     write_wavelength_grid_file(wvl_grid_file_path, wvl_array)
     
     # Initialise data array
     reflectivity = np.ones((len(wvl_array), len(phi_array), len(umu_array), 
-                             len(sza_array), len(r_eff_array), len(tau550_array), len(ic_habit_array)))
+                            len(sza_array), len(r_eff_array), 
+                            len(tau550_array), len(ic_habit_array)))
     
-    print("Start libRadtran simulation...")
         
-    # Compute vector to be passe            current_sza_start_time = timer()d to the pool.map function. Includes all relevant index combinations for 'tau550' 'r_eff'. 
+    # Compute vector to be passed to the pool.map function. Includes all 
+    # relevant index combinations for 'tau550' 'r_eff'. 
     cloud_index_array = get_index_combinations(len(r_eff_array))
     cloud_index_vector = [(line[0], line[1]) for line in cloud_index_array]
+    
+    r_eff_v, tau550_v = np.meshgrid(r_eff_array, tau550_array)
+    flat_r_eff, flat_tau550 = r_eff_v.flatten(), tau550_v.flatten()
         
     # Looping over entries in data array
+    LUTs=[]
     for ihabit in range(len(ic_habit_array)):
         ic_habit = ic_habit_array[ihabit]
         print("Computing habit: ", ic_habit)
         for isza in range(len(sza_array)):
-            current_sza_start_time = timer()
+            ziplock_args = zip(cloud_index_vector,
+                               it.repeat(wvl_array), it.repeat(phi_array), 
+                               it.repeat(umu_array), it.repeat(isza), 
+                               it.repeat(sza_array), it.repeat(r_eff_array),
+                               it.repeat(tau550_array), it.repeat(phi0), 
+                               it.repeat(cloud_top_distance),
+                               it.repeat(wvl_grid_file_path),
+                               it.repeat(ic_habit), it.repeat(ic_properties))
+            
             print("Open pool...")
             with Pool(processes = CPUs) as p:
-                # When optimizing this part: maybe you can define the zpped arguments as args = tqdm(zip(...)) and see if progress bar will show up. Do the same when including the map() function below to increase writing speed
-                uvspec_results = p.map(get_ic_reflectivity, zip(cloud_index_vector,it.repeat(wvl_array),it.repeat(phi_array),
-                                                                it.repeat(umu_array), it.repeat(isza), it.repeat(sza_array),
-                                                                it.repeat(r_eff_array), it.repeat(tau550_array), 
-                                                                it.repeat(phi0), it.repeat(cloud_top_distance),
-                                                                it.repeat(wvl_grid_file_path), it.repeat(ic_habit),
-                                                                it.repeat(ic_properties)))
+                # When optimizing this part: maybe you can define the zpped 
+                # arguments as args = tqdm(zip(...)) and see if progress bar 
+                # will show up. Do the same when including the map() function
+                # below to increase writing speed
+                uvspec_results = np.array(p.map(get_ic_reflectivity, ziplock_args))
             p.close()
+            end_of_pool_time = timer()
             print("Pool closed")
-            # Slows down process dramatically. Replace for loops with faster method, e.g. list comprehension? Would remove outer loops...
-
-            for icloud in range(len(cloud_index_array)):
+            # Slows down process dramatically. Replace for loops with faster 
+            # method, e.g. list comprehension? Would remove outer loops...
+            
+            single_results=[]
+            for icloud in range(len(flat_r_eff)):
+                print(icloud)
+                
+                single_result = xr.DataArray(
+                        data = uvspec_results[icloud,:,:,:],
+                        dims=["wvl", "umu", "phi"],
+                        coords=dict(
+                                wvl=wvl_array,
+                                umu=umu_array,
+                                phi=phi_array)
+                        ).expand_dims({
+                                "r_eff":[flat_r_eff[icloud]],
+                                "tau550":[flat_tau550[icloud]]}).rename('reflectivity')
+                single_results.append(single_result)
+                
+            merged_results=xr.merge(single_results)
+            
+            merged_results = merged_results.expand_dims({
+                            "ic_habit":[ic_habit_array[ihabit]],
+                            "sza":[sza_array[isza]]})
+            
+            LUTs.append(merged_results)
+            
+            
+            print('Rearanging output...')
+        """
+            for icloud in range(len(r_eff_v.flatten())):
                 ir_eff, itau550 = cloud_index_array[icloud]
 
                 for iwvl in range(len(wvl_array)):
                     for iumu in range(len(umu_array)):
                         for iphi in range(len(phi_array)):
-                            # Write entry. uvspec_result[iwvl, iumu*len(phi_array) + iphi + 1] Is adapted to 
-                            # specific uvspec output file format specified in input file. See template. 'icloud' iterates 
-                            # over r_eff and tau550 combinations returned by pool.map
-                            
 
-                            reflectivity[iwvl, iphi, iumu, isza, ir_eff, itau550, ihabit] = \
-                            np.array(uvspec_results)[icloud ,iwvl, iumu*len(phi_array) + iphi + 1]
+                            reflectivity[iwvl, iphi, iumu, isza, 
+                                         ir_eff, itau550, ihabit] = \
+                            uvspec_results[icloud ,iwvl, 
+                                    iumu*len(phi_array) + iphi + 1] """
     
-            print("Finished computing sza "+str(sza_array[isza]))
-            elapsed_time = (timer() - current_sza_start_time)
-            remaining_time = (len(sza_array)-(isza+1))*elapsed_time
-            print("Remaining time estimate: ", np.round(remaining_time/60., 4), "min")
-        
-        
-    end_time = timer()
-    elapsed_time = (end_time - start_time)/60.
+    LUT=xr.merge(LUTs)
     
     print("Done!")
     # Clear path
-    try: 
-        os.remove(LUTpath)
-    except:
-        pass
+    shutil.rmtree(temp_dir_path, ignore_errors=True)
     
     # Format as xr DataArray
     file = open("InputFiles/ic_input_file_template.txt", "r")
@@ -287,57 +347,37 @@ def write_icLUT(LUTpath, wvl_array, phi_array, umu_array, sza_array, r_eff_array
     file.close()
     
     print("Format results as Xarray DataArray...")
-    LUT = xr.DataArray(
         
-        data=reflectivity,
-        
-        dims=["wvl", "phi", "umu", "sza", "r_eff", "tau550", "ic_habit"],
-        
-        coords=dict(
-            wvl = wvl_array,
-            phi = phi_array,
-            umu = umu_array,
-            sza = sza_array,
-            r_eff = r_eff_array,
-            tau550 = tau550_array,
-            ic_habit = ic_habit_array),
-        
-        attrs=dict(
+    LUT.attrs=dict(    
             measurement="Reflectivity " + str(cloud_top_distance) +" km above cloud top",
             units="",
             descr=description,
-            input_template = template,)
-    )
-    
-    LUT.rename("reflectivity") #necessary? LUT = LUT.rename
+            input_template = template)
     
     LUT.wvl.attrs["units"] = r'nm'
     LUT.phi.attrs["units"] = r'degrees'
     LUT.sza.attrs["units"] = r'degrees'
     LUT.r_eff.attrs["units"] = r'$\mu $m'
+    end_time = timer()
+    elapsed_time = (end_time - start_time)/60.
+    pool_time = (end_of_pool_time - start_time)/60.
     LUT.attrs["computation_time[min]"] = elapsed_time
-    
-    if delete:
-        
-        print("Cleaning up...")
-        os.remove(wvl_grid_file_path)
-    
     print("Write LUT to netCDF file...")
-    LUT.load().to_netcdf(LUTpath)
-    f = open(LUTpath)
-    f.close()
+    save_as_netcdf(LUT, LUTpath)
     print('--------------------------------------------------------------------------------')
     print("LUT saved under"+LUTpath)    
     file_stats = os.stat(LUTpath)
     print(f'File Size in MegaBytes is {file_stats.st_size / (1024 * 1024)}')
+    print('Simulation took %f6 minutes.' %pool_time)
     print('Computation took %f6 minutes.' %elapsed_time)
     
+    print(LUT)
     return
 
-def new_write_icLUT(LUTpath, wvl_array, phi_array, umu_array, sza_array, 
-                    r_eff_array, tau550_array, ic_habit_array, phi0=0, 
-                    cloud_top_distance=1, ic_properties = "baum_v36", 
-                    CPUs=8, description=""):
+def write_icLUT(LUTpath, wvl_array, phi_array, umu_array, sza_array, 
+                r_eff_array, tau550_array, ic_habit_array, phi0=0, 
+                cloud_top_distance=1, ic_properties = "baum_v36", 
+                CPUs=8, description=""):
     
     start_time = timer()
     temp_dir_path = tempfile.mkdtemp()
@@ -371,25 +411,19 @@ def new_write_icLUT(LUTpath, wvl_array, phi_array, umu_array, sza_array,
             print("Open pool...")
             with Pool(processes = CPUs) as p:
                 # When optimizing this part: maybe you can define the zpped arguments as args = tqdm(zip(...)) and see if progress bar will show up. Do the same when including the map() function below to increase writing speed
-                uvspec_results = p.map(get_ic_reflectivity, ziplock_args)
+                uvspec_results = np.array(p.map(get_ic_reflectivity, ziplock_args))
             p.close()
             end_of_pool_time = timer()
             print("Pool closed")
-            # Slows down process dramatically. Replace for loops with faster 
-            # method, e.g. list comprehension? Would remove outer loops...
+            # Should ba faster now that some loops are removed...
 
             print('Rearanging output...')
             for icloud in range(len(cloud_index_array)):
                 ir_eff, itau550 = cloud_index_array[icloud]
 
-                for iwvl in range(len(wvl_array)):
-                    for iumu in range(len(umu_array)):
-                        for iphi in range(len(phi_array)):
-
-                            reflectivity[iwvl, iphi, iumu, isza, 
-                                         ir_eff, itau550, ihabit] = \
-                            np.array(uvspec_results)[icloud ,iwvl, 
-                                    iumu*len(phi_array) + iphi + 1] 
+                reflectivity[:, :, :, isza, 
+                             ir_eff, itau550, ihabit] = \
+                             uvspec_results[icloud] 
     
     print("Done!")
     # Clear path
